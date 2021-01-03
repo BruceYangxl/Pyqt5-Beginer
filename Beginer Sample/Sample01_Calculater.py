4502e7839689b828bb7bb9f94ade0ca835cb6076
Xianglin Yang 杨祥林 <XianglinYang@beckhoff.com>
2021-01-03 19:40:45 +0800

    Initial commit

---
+++ Beginer Sample/Sample01_Calculater.py
@@ -0,0 +1,11 @@
+import sys
+import Calculater01
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Calculater01.Ui_MainWindow()
+    ui.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())
